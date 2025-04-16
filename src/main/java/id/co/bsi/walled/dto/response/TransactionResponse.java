4f44207c3bb6436bf0670093459a11fd6a51bdd9
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-16 13:18:25 +0700

    Add register

--- src/main/java/id/co/bsi/walled/dto/response/TransactionResponse.java
+++ src/main/java/id/co/bsi/walled/dto/response/TransactionResponse.java
@@ -1,10 +1,11 @@
 package id.co.bsi.walled.dto.response;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class TransactionResponse {
-
+@EqualsAndHashCode(callSuper = true)
+public class TransactionResponse extends Response {
     private String date;
     private String type;
     private String fromTo;
