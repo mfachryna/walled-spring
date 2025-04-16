daf3a83cfe3e8f0a10608cb434db8a1629a50d7b
urboyrip <ariefrizqie18@gmail.com>
2025-04-16 15:06:35 +0700

    method dan tipe

---
+++ src/main/java/id/co/bsi/walled/dto/response/MethodResponse.java
@@ -0,0 +1,12 @@
+package id.co.bsi.walled.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class MethodResponse {
+    private int id;
+    private String type;
+}
