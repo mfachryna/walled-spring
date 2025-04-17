0f202206573a84a5f448b8bd006ef66cb01491a2
dennyrf <denirfajrin@gmail.com>
2025-04-17 11:20:26 +0700

    service user

--- src/main/java/id/co/bsi/walled/service/UserService.java
+++ src/main/java/id/co/bsi/walled/service/UserService.java
@@ -1,4 +1,26 @@
 package id.co.bsi.walled.service;
 
+import id.co.bsi.walled.dto.response.UserAccountsResponse;
+import id.co.bsi.walled.dto.response.UserDetailResponse;
+import id.co.bsi.walled.model.User;
+import id.co.bsi.walled.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserDetailResponse getUserDetail(){
+        User user = userRepository.findByEmail("test@example.com");
+        UserDetailResponse response = new UserDetailResponse(user);
+        response.setMessage("User profile retrieved");
+        return response;
+    }
+
+    public UserAccountsResponse getUserAccount() {
+        return new UserAccountsResponse(1, "706044871", "1500000");
+    }
 }
